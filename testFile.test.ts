af369229ad6b1c2f40b62b102f3e83c9939e7731
taylor greer <taylorbgreer@gmail.com>
2022-03-10 22:43:22 -0500

    Taylor and Stephanie Test

--- testFile.test.ts
+++ testFile.test.ts
@@ -7,3 +7,24 @@ const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 const myTest = new amazonPage
 
+test("Testing Add To Cart Function", async () =>{
+    await myTest.navigate()
+    await myTest.setInput(myTest.searchBarInput, "Golf balls")
+    await myTest.click(myTest.magnifyingBtn)
+    await myTest.click(myTest.golfBallResults)
+    await myTest.click(myTest.addToCartBtn)
+    let response = await myTest.getText(myTest.addToCartResults)
+    expect(response).toContain( "Added to cart")
+    
+
+});
+
+test("Testing language change", async () => {
+    await myTest.navigate()
+    await myTest.click(myTest.flagBtn)
+    await myTest.click(myTest.espanolBtn)
+    await myTest.click(myTest.saveChangesBtn)
+    let response = await myTest.getText(myTest.espanolResponse)
+    expect(response).toContain("Carrito")
+
+});
